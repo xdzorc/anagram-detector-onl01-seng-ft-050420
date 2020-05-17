d4e9f7b3c098da7ae5adb1f0a1065f5f1f8b324a
DAZUN XING <dazun.xing@gmail.com>
2020-05-17 11:40:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word =word
+  end
+  
+  def match(word_new)
+    haha=[]
+    word_new.collect do|x|
+      if x.split("").sort ==@word.split("").sort
+         haha << x
+      end
+     end
+    haha
+  end
+  
+end
+  
+  
